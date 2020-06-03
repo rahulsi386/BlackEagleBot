6dd3a99a4d6ba9f1637a99b5b7e69be2c7fc0981
Rahul Prashant Singh <rahulsi@microsoft.com>
2020-06-03 11:19:44 +0530

    Initial Bot setup

---
+++ Services/BotSettings.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Solutions;
+using BlackEagleBot.TokenExchange;
+
+namespace BlackEagleBot.Services
+{
+    public class BotSettings : BotSettingsBase
+    {
+        public TokenExchangeConfig TokenExchangeConfig { get; set; }
+    }
+}
---
+++ TokenExchange/TokenExchangeConfig.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace BlackEagleBot.TokenExchange
+{
+    public class TokenExchangeConfig : ITokenExchangeConfig
+    {
+        public string Provider { get; set; }
+
+        public string ConnectionName { get; set; }
+    }
+}
